eba0ed5045b352af3cb2b02ba0b71c060b508290
s-cryptor <grisga2001@gmail.com>
2022-06-05 01:26:28 +0300

    add state manager

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
+import * as store from './store.js'
+
 const socket = io('/')
 
 socket.on('connect', () => {
   console.log('successfully connected to wss server')
-  console.log(socket.id)
+  store.setSocketId(socket.id)
+  console.log(store.getState())
 })
