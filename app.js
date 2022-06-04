cad5731c178336f0e4f36ffea38ac317bc1be6f0
s-cryptor <grisga2001@gmail.com>
2022-06-05 00:06:52 +0300

    add assets && prepair styles

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 import express from 'express'
 import http from 'http'
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server)
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
+})
+
+let connectedPeers = []
+
+io.on('connection', (socket) => {
+  connectedPeers.push(socket.id)
+  console.log(connectedPeers)
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+
+    const newConnectedPeers = connectedPeers.filter((peerSocketId) => {
+      peerSocketId !== socket.id
+    })
+
+    connectedPeers = newConnectedPeers
+    console.log(connectedPeers)
+  })
 })
 
 server.listen(PORT, () => {
