e6b2d438307effd3bcf47083f5ac2d5b28a99fba
s-cryptor <grisga2001@gmail.com>
2022-06-09 15:54:05 +0300

    add copy button func

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,9 @@ import * as wss from './wss.js'
 
 const socket = io('/')
 wss.registerSocketEvents(socket)
+
+const personalCodeCopyButton = document.getElementById('personal_code_copy_button')
+personalCodeCopyButton.addEventListener('click', () => {
+  const personalCode = store.getState().socketId
+  navigator.clipboard && navigator.clipboard.writeText(personalCode)
+})
